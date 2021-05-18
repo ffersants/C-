15ae0c3022f642517afc4e223869a453d9cdb693
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-05-18 01:18:53 -0300

    First commit. Hello world com c#.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float test = 5f / 2;
+            Console.WriteLine(test);
+            Console.ReadLine();
+        }
+    }
+}
