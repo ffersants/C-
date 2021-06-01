6e098916d35162136dee481bccdbf5067d9702be
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-05-31 23:13:14 -0300

    Casting, conversão implícita e divisão entre inteiros.

---
+++ Udemy/Unidade 3/9 - Conversão implícita e casting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Conversão_implícita_e_casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tudo ok - 32bits (int) cabem em 64 (double)
+            int a = 10;
+            double b = a;
+            Console.WriteLine(b);
+
+            //não é possível converter implicitamente, logo, deve-se realizar um casting
+            double x = 50;
+            int y = (int)x;
+            Console.WriteLine(y);
+
+            //divisão entre inteiros resulta em inteiro
+            int d = 5;
+            int e = 2;
+            //casting para forçar a presença da casa decimal... Int / double = double
+            Console.WriteLine((double)d/e);
+        }
+    }
+}
