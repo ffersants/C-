028bf3d4e9e8a0ba6b4c9602d90207bcc76f9708
Fernando Ferreira - HPZ4 <fersants.nando.ferreira@gmail.com>
2021-06-08 18:46:07 -0300

    Updates.

--- Udemy/Unidade 6/Lista exercise/Employe.cs
+++ Udemy/Unidade 6/Lista exercise/Employe.cs
@@ -8,5 +8,15 @@ namespace Lista_exercise
 {
     class Employe
     {
+        public int id { get; private set; }
+        public string name { get; private set; }
+        public double salaray { get; private set; }
+
+        public Employe(int id, string name, double salaray)
+        {
+            this.id = id;
+            this.name = name;
+            this.salaray = salaray;
+        }
     }
 }
--- Udemy/Unidade 6/Lista exercise/Program.cs
+++ Udemy/Unidade 6/Lista exercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lista_exercise
 {
@@ -6,7 +7,31 @@ namespace Lista_exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List < Employe > employees = new List<Employe>();
+            Console.Write("How many employees will be registrered? ");
+            int[] totalEmployes = new int[int.Parse(Console.ReadLine())];
+
+            for(int i = 0; i < totalEmployes.Length; i++)
+            {
+                Console.WriteLine("Eployee #" + (i + 1));
+                
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                employees.Add(new Employe(id, name, salary));
+                
+            }
+
+            Console.Write("Enter the employee id that will have salary increased: ");
+            int employeeID = int.Parse(Console.ReadLine());
+
+            employees.Find(x => x.id == employeeID);
         }
     }
 }
