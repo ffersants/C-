ac8fc86ccd15fdd903653238fb986acf1e4b2b05
Fernando Ferreira - HPZ4 <fersants.nando.ferreira@gmail.com>
2021-06-07 21:45:46 -0300

    Modificador de parametro ref.

---
+++ Udemy/Unidade 6/Modificado de parametro - Ref/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modificado_de_parametro___Ref
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toBeTripled = 50;
+            Calculadora.Triple(ref toBeTripled);
+            Console.WriteLine(toBeTripled);
+        }
+    }
+}
