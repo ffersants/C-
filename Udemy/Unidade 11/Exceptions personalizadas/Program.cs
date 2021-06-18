94aa3b51e6c0b436e233aacafa395e941afca83c
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-06-18 00:20:07 -0300

    Realizar exercício.

---
+++ Udemy/Unidade 11/Exceptions personalizadas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exceptions_personalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
