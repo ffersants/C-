8adbade73081fa56bec7eda0f75835230330d86f
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-06-27 19:30:10 -0300

    Começando a tratativa de arquivos.

---
+++ Udemy/Unidade 13/File e FileInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+namespace File_e_FileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FileInfo txt1 = new FileInfo(@"C:\Users\DheyEs\Videos\teste1.txt");
+                txt1.CopyTo(@"C:\Users\DheyEs\Videos\teste2.txt");
+            }
+
+            catch(SystemException e)
+            {
+                Console.WriteLine("Opa... Deu erro aqui!");
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
