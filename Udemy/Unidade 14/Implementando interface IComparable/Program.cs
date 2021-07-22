35ce514ca9f226d5e4302c8ff7550cf104e3e984
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-07-21 21:44:14 -0300

    Lidando com leitura de txt e operador Sort.

---
+++ Udemy/Unidade 14/Implementando interface IComparable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Implementando_interface_IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\DheyEs\Documents\C#\Udemy\Unidade 14\Implementando interface IComparable\test.txt";
+            using (StreamReader sr = File.OpenText(path)) 
+            {
+
+                List<string> Pessoas = new List<string>();
+                while (!sr.EndOfStream)
+                {
+                    Pessoas.Add(sr.ReadLine());
+                }
+                Pessoas.Sort();
+                Pessoas.ForEach(i => Console.Write("{0}\n", i));
+
+            }
+        }
+    }
+}
