acfae018465d381149d5824cd837547c2c7bcc4c
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-07-21 23:41:21 -0300

    Sobrecarga no método Sort, passando Comparison com expressão lambada para ordenar lista.

---
+++ Udemy/Unidade 15/Sobrecarga Sort com método delegado/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecarga_Sort_com_método_delegado
+{
+    class Produto
+    {
+        public string name;
+        public double price;
+        public bool availble;
+
+        public Produto(string name, double price, bool availble)
+        {
+            this.name = name;
+            this.price = price;
+            this.availble = availble;
+        }
+
+        public override string ToString()
+        {
+            return $"\nProduto: {name} \nPreco: {price} \nDisponivel: {availble}\n\n";
+        }
+    }
+}
