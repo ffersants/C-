a8b3b3e6a521332d4dfa3ef7a649b91a9f51e444
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-07-24 20:28:33 -0300

    Delegate, delegate multicast com metodo invoke e funcao que trabalha com parametros e resolve delegate.

---
+++ Udemy/Unidade 16/Delegate - Multicast e funcao invoke/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegate___Multicast_e_funcao_invoke
+{
+    class Program
+    {
+        public delegate void ConsoleActions();
+        static void Main(string[] args)
+        {
+            ConsoleActions delegateFunc = new ConsoleActions(method1);
+            delegateFunc += method2;
+            delegateFunc.Invoke();
+        }
+        static void method1()
+        {
+            Console.WriteLine("Executado método 1");
+        }
+        static void method2()
+        {
+            Console.WriteLine("Executado método 2");
+        }
+    }
+}
