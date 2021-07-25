e51d6c9fb6d0622f117e6f5048d2c19c1b442c23
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-07-24 21:03:20 -0300

    Predicate com o método removeAll para excluir, de uma determinada lista, itens que testarem true para verificação do predicate.

---
+++ Udemy/Unidade 16/Predicate - Método removeAll()/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Predicate___Método_removeAll__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> produtos = new List<Product>();
+
+            produtos.Add(new Product("Televisão", 5000));
+            produtos.Add(new Product("Bike", 1200));
+            produtos.Add(new Product("Lâmpada", 20.50));
+            produtos.Add(new Product("Armário", 800));
+
+            produtos.RemoveAll((produto) => produto.price > 1000);
+
+            produtos.ForEach(product => Console.WriteLine(product.ToString()));
+        }
+    }
+}
