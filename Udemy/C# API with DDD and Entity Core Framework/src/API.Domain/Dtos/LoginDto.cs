578e7cc6057d32eb169272718713714ebbd67466
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2021-09-07 23:29:03 -0300

    Realizada configuração para token na domain, data e appsetings.json

---
+++ Udemy/C# API with DDD and Entity Core Framework/src/API.Domain/Dtos/LoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos
+{
+    class LoginDto
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
+        [StringLength(100, ErrorMessage = "O máximo para esse campo é de 100 caracteres.")]
+        public string email { get; set; }
+    }
+}
---
+++ Udemy/C# API with DDD and Entity Core Framework/src/API.Domain/Security/SigningConfigurations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Security
+{
+    class SigningConfigurations
+    {
+        //Esse atributo é do tipo SecurityKey, que é a classe para receber chaves de segurança
+        public SecurityKey Key { get; set; }
+        //recebe a chave e o algoritmo de assinatura da chave a ser aplicado
+        public SigningCredentials SigningCredentials { get; set; }
+        public SigningConfigurations()
+        {
+            //initializes a class with a random key pair of the specified key size.
+            using (var provider = new RSACryptoServiceProvider(2048))
+            {
+                //Representa uma chave de segurança que é gerada usando o algoritmo RSA.
+                Key = new RsaSecurityKey(provider.ExportParameters(true));
+            }
+            //chave e algoritmo de assinatura a ser aplicado
+            SigningCredentials = new SigningCredentials(Key, SecurityAlgorithms.RsaSha256Signature);
+        }
+
+
+
+    }
+}
